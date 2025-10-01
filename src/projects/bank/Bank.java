b49e2b5049ab48ddb9450c19e19392867e6c5679
EthanCMSC <w1227762@myfcc.frederick.edu>
2025-10-01 19:26:52 +0000

    Followed feedback messages. Haven't tested myself yet; will do that later

--- src/projects/bank/Bank.java
+++ src/projects/bank/Bank.java
@@ -8,57 +8,80 @@ public class Bank
     // Constructors
     public Bank()
     {
-        this.accounts = new Account[0];
+        this.accounts = new Account[100];
     }
 
     // Instance methods
-    // TODO javadoc
-    public boolean add(Account newAcct) // Adds a new account to bank. New account's ID must be unique. Returns true if successful; returns false if failed.
+    /**
+     * Adds a new account to bank. New account's ID must be unique. Returns true if successful; returns false otherwise.
+     * @param newAcct - The new Account to add
+     * @return - true if account was successfully added; false otherwise
+     */
+    public boolean add(Account newAcct)
     {
-        // Create new accounts array
-        Account[] temp = new Account[this.accounts.length + 1];
-        
-        // Copy existing accounts to new array TODO reconsider strategy
-        for (int i = 0; i < this.accounts.length; i ++)
+        // Check if account ID is unique
+        if (this.find(newAcct.getID()) == -1)
         {
-            // Check that new account ID is unique TODO use find method
-            if (!newAcct.getID().equals(this.accounts[i].getID()))
+            for (int i = 0; i < accounts.length; i ++)
+            {
+                // Add account to current array if free slot exists
+                if (accounts[i] == null)
+                {
+                    accounts[i] = newAcct;
+                    return true;
+                }
+            }
+            // Copy accounts to larger array and add new account there if overflow occurs
+            Account[] temp = new Account[this.accounts.length + 100];
+            for (int i = 0; i < this.accounts.length; i ++)
             {
                 temp[i] = this.accounts[i];
             }
-            // Cancel account creation and return false if ID is in use
-            else
-            {
-                return false;
-            }
+            temp[this.accounts.length] = newAcct;
+            this.accounts = temp;
+            return true;
         }
-
-        // Add new account at end of new array
-        temp[temp.length - 1] = newAcct;
-
-        // Save new array to accounts variable and return true
-        this.accounts = temp;
-        return true;
+        else
+        {
+            // Return false if account ID is not unique
+            return false;
+        }
     }
 
-    // TODO change return value to match spec
-    public Account find(String acctID) // Finds a bank account with the specified ID. Returns the account if it exists; returns null otherwise.
+    /**
+     * Returns the index of a specified account by its ID if it exists.
+     * @param acctID - The ID of the account to search for
+     * @return - Account's index in accounts array if it exists; -1 otherwise
+     */
+    public int find(String acctID)
     {
         // Iterate through existing accounts
-        for (Account acct : this.accounts)
+        for (int i = 0; i < this.accounts.length; i ++)
         {
             // Return account if ID matches
-            if (acct.getID().equals(acctID))
+            if (this.accounts[i].getID().equals(acctID))
             {
-                return acct;
+                return i;
             }
         }
-        // Return null if account was not found
-        return null; // noooooooooo
+        // Return -1 if account was not found
+        return -1;
     }
 
-    public int getCount() // Returns an int value equal to the number of accounts stored in the bank.
+    /**
+     * Returns the number of accounts in the bank
+     * @return - int equal to number of accounts in bank
+     */
+    public int getCount()
     {
-        return this.accounts.length;  // TODO return correct value
+        for (int i = 0; i < this.accounts.length; i ++)
+        {
+            if (this.accounts[i] == null)
+            {
+                return i;
+            }
+        }
+
+        return this.accounts.length;
     }
 }
--- test/projects/bank/BankTest.java
+++ test/projects/bank/BankTest.java
@@ -2,5 +2,34 @@ package projects.bank;
 
 public class BankTest
 {
-    // TODO add coverage
+    public static void main(String[] args)
+    {
+        // Create Bank object
+        Bank bank = new Bank();
+
+        // Test add() method (success w/o overflow)
+        System.out.println(bank.add(new Account("JohnDoeChecking", "John Doe", 1000.00, AccountType.CHECKING))); // Should print "true"
+        System.out.println(bank.add(new Account("JaneDoeSavings", "Jane Doe", 1000.00, AccountType.SAVINGS))); // Should print "true"
+        
+        // Test add() method (failure)
+        System.out.println(bank.add(new Account("JohnDoeChecking", "Fake Name", 1000.00, AccountType.CHECKING))); // Should print "false"
+        
+        // Test find() method (success)
+        System.out.println(bank.find("JohnDoeChecking")); // Should print "0"
+        System.out.println(bank.find("JaneDoeSavings")); // Should print "1"
+        
+        // Test add() method (success w/ overflow)
+        for (int i = 0; i < 98; i ++)
+        {
+            bank.add(new Account("placeholder" + i, "placeholder", 0.00, AccountType.CHECKING)); // Add placeholder accounts until there are 100 accounts in the bank
+        }
+        System.out.println(bank.add(new Account("overflowtest", "Overflow test", 0.00, AccountType.CHECKING))); // Should print "true"
+
+        // Test find() method (failure)
+        System.out.println(bank.find("JaneDoeChecking")); // Should print "-1"
+        System.out.println(bank.find("JohnDoeSavings")); // Should print "-1"
+
+        // Test getCount() method
+        System.out.println(bank.getCount()); // Should print "101"
+    }
 }
