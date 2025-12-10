4039fc6a578fb4a46ea8fb050f719b8f972d434b
EthanCMSC <w1227762@myfcc.frederick.edu>
2025-12-10 20:29:39 +0000

    Updated main method to test all sample mazes and log return values of depthFirstSolve() methods.

--- src/projects/maze/Main.java
+++ src/projects/maze/Main.java
@@ -17,14 +17,24 @@ public class Main
 
     private static void phase2()
     {
-        Maze solvable = MazeReader.load("data/sample_maze.txt");
+        Maze solvable1 = MazeReader.load("data/sample_maze.txt");
         System.out.println("Maze successfully loaded!");
-        solvable.depthFirstSolve();
-        solvable.serialize("data/sample_maze_out.txt");
+        System.out.println("Solve attempt returned " + solvable1.depthFirstSolve() + ".");
+        solvable1.serialize("data/sample_maze_out.txt");
+
+        Maze solvable2 = MazeReader.load("data/sample_maze2.txt");
+        System.out.println("Maze successfully loaded!");
+        System.out.println("Solve attempt returned " + solvable2.depthFirstSolve() + ".");
+        solvable2.serialize("data/sample_maze2_out.txt");
+
+        Maze solvable3 = MazeReader.load("data/sample_maze3.txt");
+        System.out.println("Maze successfully loaded!");
+        System.out.println("Solve attempt returned " + solvable3.depthFirstSolve() + ".");
+        solvable3.serialize("data/sample_maze3_out.txt");
 
         Maze unsolvable = MazeReader.load("data/sample_maze_no_solution.txt");
         System.out.println("Maze successfully loaded!");
-        unsolvable.depthFirstSolve();
+        System.out.println("Solve attempt returned " + unsolvable.depthFirstSolve() + ".");
         unsolvable.serialize("data/sample_maze_no_solution_out.txt");
     }
 }
